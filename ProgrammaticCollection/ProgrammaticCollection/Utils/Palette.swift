9673b44ec5203f1d17bc888f72428eb136272b2c
Emanuele Garolla <e.garolla@reply.it>
2020-11-22 23:33:15 +0100

    A bit of darkmode

--- ProgrammaticCollection/ProgrammaticCollection/Utils/Palette.swift
+++ ProgrammaticCollection/ProgrammaticCollection/Utils/Palette.swift
@@ -9,8 +9,8 @@ import UIKit
 
 extension UIColor {
     static let primary = UIColor(named: "tripadvisor_green")
-    static let primaryText = UIColor.black
-    static let primaryBackground = UIColor.white
+    static let primaryText = UIColor(named: "darkgray_white")
+    static let primaryBackground = UIColor(named: "white_darkgray")
     static let secondaryBackground = UIColor(named: "tripadvisor_gray")
     static let tertiaryBackground = UIColor(named: "tripadvisor_light_green")
     static let quartiaryBackground = UIColor.black
